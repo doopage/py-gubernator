a6c91ee8406cd410c1afe30542174dd38a9edc59
Nobi <nobi@nobidev.com>
2022-11-01 09:59:48 +0000

    Fix build with --use-pep517 (poetry)

--- setup.py
+++ setup.py
@@ -16,44 +16,65 @@
 # limitations under the License.
 #
 
-try:  # for pip >= 10
-    from pip._internal.req import parse_requirements
-except ImportError:  # for pip <= 9.0.3
-    from pip.req import parse_requirements
+try:
+    try:  # for pip >= 10
+        from pip._internal.req import parse_requirements
+    except ImportError:  # for pip <= 9.0.3
+        from pip.req import parse_requirements
+    support_pip_parse_requirements = True
+except ModuleNotFoundError:
+    support_pip_parse_requirements = False
+
 from setuptools import setup, find_packages
 import platform
 
-if platform.python_version_tuple()[0] == '2':
-    reqs = parse_requirements('requirements-py2.txt', session='')
-else:
-    reqs = parse_requirements('requirements-py3.txt', session='')
 
-# Generator must be converted to list, or we will only have one chance to read each element, meaning that the first requirement will be skipped.
-reqs = list(reqs)
-try:
-    requirements = [str(r.req) for r in reqs]
-except:
-    requirements = [str(r.requirement) for r in reqs]
+def get_requirements():
+    if platform.python_version_tuple()[0] == "2":
+        requirements_txt = "requirements-py2.txt"
+    else:
+        requirements_txt = "requirements-py3.txt"
+
+    if support_pip_parse_requirements:
+        reqs = parse_requirements(requirements_txt, session="")
+
+        # Generator must be converted to list, or we will only have one chance to read each element, meaning that the first requirement will be skipped.
+        reqs = list(reqs)
+        try:
+            return [str(r.req) for r in reqs]
+        except:
+            return [str(r.requirement) for r in reqs]
+    else:
+        # python3 -m pip install --disable-pip-version-check --upgrade --no-deps . --use-pep517
+        # https://stackoverflow.com/questions/49689880/proper-way-to-parse-requirements-file-after-pip-upgrade-to-pip-10-x-x
+        import pkg_resources
+
+        reqs = []
+        with open(requirements_txt) as f:
+            for req in pkg_resources.parse_requirements(f.read()):
+                reqs.append(str(req))
+        return reqs
+
 
 setup(
-    name='gubernator',
-    version='v1.0.0-rc.8',
+    name="gubernator",
+    version="v1.0.0-rc.8",
     description="Python client for gubernator",
     author="Derrick J. Wippler",
-    author_email='thrawn01@gmail.com',
-    url='https://github.com/mailgun/gubernator',
-    package_dir={'': '.'},
-    packages=find_packages('.', exclude=['tests']),
-    install_requires=requirements,
+    author_email="thrawn01@gmail.com",
+    url="https://github.com/mailgun/gubernator",
+    package_dir={"": "."},
+    packages=find_packages(".", exclude=["tests"]),
+    install_requires=get_requirements(),
     license="Apache Software License 2.0",
-    python_requires='>=2.7',
+    python_requires=">=2.7",
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: Apache Software License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
     ],
 )
